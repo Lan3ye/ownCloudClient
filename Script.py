949d9758ffa87dece2c30269370e9f1c14aeb6f1
Janik Krause <janik.krause2002@gmail.com>
2024-05-22 14:00:23 +0200

    Started working on analyzing the file structure

--- Script.py
+++ Script.py
@@ -1,6 +1,7 @@
 import requests
 import requests.auth
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup as BS
 
 uname = "OC_User_1" 
 passwd = 'S0vrpkQ/jItB}u1O6"@<Kh'
@@ -10,13 +11,18 @@ url = f"http://52.164.245.91/remote.php/dav/files/{uname}"
 def authenticate(url, auth):
     headers = {'Depth': '1'} # Setting headers for the PROPFIND request
     response = requests.request('PROPFIND', url, auth=auth, headers=headers)
-    print(response.status_code)
+    # print(response.status_code)
     
     if response.status_code == 207:
-       print(response.text)
+        # print(response.text)
+        data = response.text
+        soup = BS(data, features="xml")
+        hrefs = soup.find_all('d:href')
+        print(hrefs)
+    else:
+        print(f'Request failed with code {response.status_code}.')
     # print(response.text)
-    
     
 
 auth = requests.auth.HTTPBasicAuth(uname, passwd)
-print(authenticate(url, auth))
+authenticate(url, auth)
