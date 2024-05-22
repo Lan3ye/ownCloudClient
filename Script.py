a4e06e0a08d07c4656c8239e430fa5d7d9934dd5
Janik Krause <janik.krause2002@gmail.com>
2024-05-22 12:27:51 +0200

    Communication with the WebDAV-Server now working

--- Script.py
+++ Script.py
@@ -1,7 +1,22 @@
 import requests
+import requests.auth
+import xml.etree.ElementTree as ET
 
-print("Username:")
-uname = input()
-print("Password:")
-passwd = input()
+uname = "OC_User_1" 
+passwd = 'S0vrpkQ/jItB}u1O6"@<Kh'
 
+url = f"http://52.164.245.91/remote.php/dav/files/{uname}"
+
+def authenticate(url, auth):
+    headers = {'Depth': '1'} # Setting headers for the PROPFIND request
+    response = requests.request('PROPFIND', url, auth=auth, headers=headers)
+    print(response.status_code)
+    
+    if response.status_code == 207:
+       print(response.text)
+    # print(response.text)
+    
+    
+
+auth = requests.auth.HTTPBasicAuth(uname, passwd)
+print(authenticate(url, auth))
