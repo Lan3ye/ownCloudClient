8631db98feda361749fcd6463554fb1c255f07c5
Janik Krause <janik.krause2002@gmail.com>
2024-05-23 12:06:51 +0200

    Added error handling

--- Script.py
+++ Script.py
@@ -10,20 +10,22 @@ url = f"http://52.164.245.91/remote.php/dav/files/{uname}"
 
 def authenticate(url, auth):
     headers = {'Depth': '1'} # Setting headers for the PROPFIND request
-    response = requests.request('PROPFIND', url, auth=auth, headers=headers)
-    # print(response.status_code)
-    
-    if response.status_code == 207:
-        # print(response.text)
-        data = response.text
-        soup = BS(data, features="xml")
-        hrefs = soup.find_all('d:href')
-        for href in hrefs:
-            print(href)
-    else:
-        print(f'Request failed with code {response.status_code}.')
-    # print(response.text)
-    
+    try:
+        response = requests.request('PROPFIND', url, auth=auth, headers=headers)
+        # print(response.status_code)
+        if response.status_code == 207:
+            # print(response.text)
+            data = response.text
+            soup = BS(data, features="xml")
+            hrefs = soup.find_all('d:href')
+            for href in hrefs:
+                print(href)
+        else:
+            print(f'Request failed with code {response.status_code}.')
+            # print(response.text)
+    except Exception as e:
+        print(f'An error occured {e}')
+        
 
 auth = requests.auth.HTTPBasicAuth(uname, passwd)
 authenticate(url, auth)
