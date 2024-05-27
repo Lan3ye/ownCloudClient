51d6ce930b27697df750647456864123396a8cf9
Janik Krause <janik.krause2002@gmail.com>
2024-05-27 16:22:32 +0200

    Script now recognizes client and server timezones and incorporates them into the calculations.

--- Script.py
+++ Script.py
@@ -3,18 +3,19 @@ import requests.auth
 import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup as BS
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import os
+import pytz
+import tzlocal
 
 uname = "OC_User_1" 
 passwd = 'S0vrpkQ/jItB}u1O6"@<Kh'
-
 url = f"http://52.164.245.91/remote.php/dav/files/{uname}"
-
 localPath = "testdir/"
+clientTZ = tzlocal.get_localzone()
 
 def getRemoteFiles(url, auth):
-    tempDF = pd.DataFrame(columns=['Path', 'LastMod', 'Type', 'Size', 'Quota', 'ETag'])
+    tempDF = pd.DataFrame(columns=['Path', 'LastMod', 'Type', 'Size']) #, 'Quota', 'ETag'])
     headers = {'Depth': '50'} # Setting headers for the PROPFIND request
     # try:
     answer = requests.request('PROPFIND', url, auth=auth, headers=headers)
@@ -31,22 +32,23 @@ def getRemoteFiles(url, auth):
         for response in responses:
             path = response.find('href').text
             path = path.replace('/remote.php/dav/files/OC_User_1', "")
-            lastMod = response.find('getlastmodified').text
-            lastMod = datetime.strptime(lastMod, format_str)
-            eTag = response.find('getetag').text
+            lastModStr = response.find('getlastmodified').text
+            lastMod = datetime.strptime(lastModStr, format_str)
+            lastMod = lastMod.replace(tzinfo=pytz.UTC)
+            # eTag = response.find('getetag').text
 
             # Checks whether entry is a Directory or a file
             if response.find('collection') != None:
                 size = response.find('quota-used-bytes').text
-                quota = response.find('quota-available-bytes').text
+                # quota = response.find('quota-available-bytes').text
                 resType = 'Directory'
                 # print(resType)
             else:
                 size = response.find('getcontentlength').text
-                quota = None
+                # quota = None
                 resType = 'File'
 
-            tempDF.loc[index] = [path, lastMod, resType, size, quota, eTag]
+            tempDF.loc[index] = [path, lastMod, resType, size] #, quota, eTag]
             index = index + 1
 
         # print(tempDF)
@@ -58,7 +60,7 @@ def getRemoteFiles(url, auth):
         # print(f'An error occured {e}')
 
 def getLocalFiles(localPath):
-    tempDF = pd.DataFrame(columns=['Path', 'LastMod', 'Size'])
+    tempDF = pd.DataFrame(columns=['Path', 'LastMod', 'Type', 'Size'])
     localFiles = os.listdir(localPath)
     index = 0
     for file in localFiles:
@@ -67,18 +69,48 @@ def getLocalFiles(localPath):
             size = os.path.getsize(fullPath)
             path = fullPath
             lastMod = datetime.fromtimestamp(os.path.getmtime(fullPath))
-            tempDF.loc[index] = [path, lastMod, size]
+            lastMod = lastMod.replace(tzinfo=clientTZ)
+            resType = 'File'
+            tempDF.loc[index] = [path, lastMod, resType, size]
+            index = index + 1
+        elif os.path.isdir(fullPath):
+            size = os.path.getsize(fullPath)
+            path = fullPath
+            lastMod = datetime.fromtimestamp(os.path.getmtime(fullPath))
+            lastMod = lastMod.replace(tzinfo=clientTZ)
+            tempDF.loc[index] = [path, lastMod, resType, size]
+            resType = 'Directory'
             index = index + 1
     
     return tempDF
 
+def syncToCloud(remoteFiles, localFiles):
+    print('syncToCloud')
+def syncToDesktop(remoteFiles, localFiles):
+    # 1. Check whether files exist in both DFs
+    print('syncToDesktop')
 
-# auth = requests.auth.HTTPBasicAuth(uname, passwd)
-# remoteFiles = getRemoteFiles(url, auth)
+auth = requests.auth.HTTPBasicAuth(uname, passwd)
+remoteFiles = getRemoteFiles(url, auth)
+localFiles = getLocalFiles(localPath)
+
+lastRemoteChange = remoteFiles['LastMod'].max()
+lastLocalChange = localFiles['LastMod'].max()
+# lastLocalChange = int(localFiles['LastMod'].max().timestamp())
+
+print(lastRemoteChange)
+print(lastLocalChange)
+
+syncCheck = lastRemoteChange - lastLocalChange
+
+
+print(syncCheck)
+if syncCheck > timedelta(seconds=10):
+    syncToDesktop(remoteFiles, localFiles)
+elif syncCheck < timedelta(seconds=-10):
+    syncToCloud(remoteFiles, localFiles)
 
 # # for index, row in remoteFiles.iterrows():
 # #     print(f"Row {index}: {row.to_dict()}")
 
-localFiles = getLocalFiles(localPath)
 
-print(localFiles)
