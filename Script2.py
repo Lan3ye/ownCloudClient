12af5bb098a0d5d29bef8d02cb6afccea9a5041e
Janik Krause <janik.krause2002@gmail.com>
2024-05-27 13:49:22 +0200

    Tried some things with the webdav3 api, but I think I'll stick with my own implementation.

--- Script2.py
+++ Script2.py
@@ -1,4 +1,6 @@
 from webdav3.client import Client
+import time
+import os
 
 uname = "OC_User_1" 
 passwd = 'S0vrpkQ/jItB}u1O6"@<Kh'
@@ -12,5 +14,23 @@ options = {
 
 client = Client(options)
 
-files = client.list()
-print(files)
+remote_files = client.list()
+print(remote_files)
+# file_info = client.info(remote_files[0])
+
+
+def get_remote_files(client):
+    """Return a dictionary with remote file paths and their modification times."""
+    remote_files = {}
+    items = client.list()
+    for item in items:
+        if client.check(item):
+            continue  # Skip directories
+        info = client.info(item)
+        mod_time = info.get('getlastmodified')
+        mod_time = time.mktime(time.strptime(mod_time, '%a, %d %b %Y %H:%M:%S %Z'))
+        relative_path = os.path.relpath(item)
+        remote_files[relative_path] = mod_time
+    return remote_files
+
+print(get_remote_files(client))
